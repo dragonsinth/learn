1293c58d5d789147420aa3fad426fba82133ad47
Scott Blum <dragonsinth@gmail.com>
2021-12-02 07:57:47 -0500

    fmt and clean up aoc 1

--- aoc/01/main/data.go
+++ aoc/01/main/data.go
@@ -1,16 +1,4 @@
 package main
 
-var sample = `
-199
-200
-208
-210
-200
-207
-240
-269
-260
-263
+var real = `
 `
-
-var real = ``
--- aoc/01/main/main.go
+++ aoc/01/main/main.go
@@ -6,7 +6,20 @@ import (
 	"strings"
 )
 
-func main() {
+var sample = `
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+`
+
+func main1() {
 	input := parse(sample)
 	count := 0
 
@@ -27,17 +40,43 @@ func main() {
 	fmt.Println(count)
 }
 
+
+func main() {
+	input := parse(sample)
+	count := 0
+
+	last := 0
+	for i := 2; i < len(input); i++ {
+		sum := input[i-2] + input[i-1] + input[i]
+		if i > 2 {
+			fmt.Print(i, last, sum)
+			if last < sum {
+				fmt.Println(" increase")
+				count = count + 1
+			} else if last > sum {
+				fmt.Println(" decrease")
+			} else {
+				fmt.Println(" same")
+			}
+		}
+		last = sum
+
+	}
+
+	fmt.Println(count)
+}
+
 func parse(in string) []int {
 	var ret []int
-	split := strings.Split(in, "\n")
-	for _, s := range split {
-		s = strings.TrimSpace(s)
-		if s == "" {
+	lines := strings.Split(in, "\n")
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if line == "" {
 			continue
 		}
-		n, err := strconv.Atoi(s)
+		n, err := strconv.Atoi(line)
 		if err != nil {
-			panic(fmt.Sprint(s, err))
+			panic(fmt.Sprint(line, err))
 		}
 		ret = append(ret, n)
 	}
