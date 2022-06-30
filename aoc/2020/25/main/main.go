5e8347f79c119dcf47cf208e6b93a130e26f5bf2
Scott Blum <dragonsinth@gmail.com>
2022-06-29 21:37:48 -0400

    aoc 25 2020 -- optimize

--- aoc/2020/25/main/main.go
+++ aoc/2020/25/main/main.go
@@ -16,9 +16,9 @@ const (
 func main() {
 	secrets := []int{0, 0}
 	found := 0
+
+	v := 1
 	for i := 0; found < 2; i++ {
-		v := transform2(i, keyRoot)
-		//fmt.Println(i, v, transform2(i, keyRoot))
 		if v == input[0] {
 			//fmt.Println(0, i)
 			secrets[0] = i
@@ -29,6 +29,7 @@ func main() {
 			secrets[1] = i
 			found++
 		}
+		v = (v * keyRoot) % modulus
 	}
 
 	fmt.Println(secrets)
@@ -38,14 +39,6 @@ func main() {
 }
 
 func transform(secret int, in int) int {
-	v := 1
-	for i := 0; i < secret; i++ {
-		v = (v * in) % modulus
-	}
-	return v
-}
-
-func transform2(secret int, in int) int {
 	ret := 1
 	for secret > 0 {
 		if secret&1 != 0 {
