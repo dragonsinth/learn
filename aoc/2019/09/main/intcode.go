3987e192d1a990f441ef9137958b19e12e0ee817
Scott Blum <dragonsinth@gmail.com>
2022-07-07 13:13:01 -0400

    aoc 9 2019: refactor IntCodeMachine for later

---
+++ aoc/2019/09/main/intcode.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func NewIntMachine(intCodes []int) *IntMachine {
+	p := &IntMachine{
+		pc:     0,
+		rel:    0,
+		mem:    make([]int, allocSize(len(intCodes))),
+		input:  make(chan int),
+		output: make(chan int),
+	}
+	copy(p.mem, intCodes)
+	return p
+}
+
+func allocSize(min int) int {
+	ret := 1024
+	for ret < min {
+		ret <<= 1
+	}
+	return ret
+}
+
+type IntMachine struct {
+	pc     int // program counter
+	rel    int // relative base
+	mem    []int
+	input  chan int
+	output chan int
+}
+
+func (m *IntMachine) Writer() chan<- int {
+	return m.input
+}
+
+func (m *IntMachine) Reader() <-chan int {
+	return m.output
+}
+
+func (m *IntMachine) Run() {
+	defer close(m.output)
+	for {
+		op := m.mem[m.pc]
+		m.pc++
+		if op == 99 {
+			return
+		}
+		switch op % 100 {
+		case 1:
+			a := m.load(op / 100)
+			b := m.load(op / 1000)
+			c := m.addr(op / 10000)
+			*c = a + b
+		case 2:
+			a := m.load(op / 100)
+			b := m.load(op / 1000)
+			c := m.addr(op / 10000)
+			*c = a * b
+		case 3:
+			c := m.addr(op / 100)
+			*c = <-m.input
+		case 4:
+			a := m.load(op / 100)
+			m.output <- a
+		case 5:
+			a := m.load(op / 100)
+			b := m.load(op / 1000)
+			if a != 0 {
+				m.pc = b
+			}
+		case 6:
+			a := m.load(op / 100)
+			b := m.load(op / 1000)
+			if a == 0 {
+				m.pc = b
+			}
+		case 7:
+			a := m.load(op / 100)
+			b := m.load(op / 1000)
+			c := m.addr(op / 10000)
+			*c = boolVal(a < b)
+		case 8:
+			a := m.load(op / 100)
+			b := m.load(op / 1000)
+			c := m.addr(op / 10000)
+			*c = boolVal(a == b)
+		case 9:
+			a := m.load(op / 100)
+			m.rel += a
+		default:
+			panic(op)
+		}
+	}
+}
+
+func (m *IntMachine) load(op int) int {
+	v := m.mem[m.pc]
+	m.pc++
+	switch op % 10 {
+	case 0:
+		return *m.ensureMem(v)
+	case 1:
+		return v
+	case 2:
+		return *m.ensureMem(v + m.rel)
+	default:
+		panic(op)
+	}
+}
+
+func (m *IntMachine) addr(op int) *int {
+	a := m.mem[m.pc]
+	m.pc++
+	switch op % 10 {
+	case 0:
+		return m.ensureMem(a)
+	case 2:
+		return m.ensureMem(a + m.rel)
+	default:
+		panic(op)
+	}
+}
+
+func (m *IntMachine) ensureMem(addr int) *int {
+	if addr >= len(m.mem) {
+		// realloc
+		newBuf := make([]int, allocSize(addr))
+		copy(newBuf, m.mem)
+		m.mem = newBuf
+	}
+	return &m.mem[addr]
+}
+
+func boolVal(v bool) int {
+	if v {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func parseIntCodes(line string) []int {
+	vals := strings.Split(line, ",")
+	var codes []int
+	for _, v := range vals {
+		codes = append(codes, mustInt(v))
+	}
+	return codes
+}
+
+func mustInt(s string) int {
+	if v, err := strconv.Atoi(s); err != nil {
+		panic(fmt.Sprint(s, err))
+	} else {
+		return v
+	}
+}
--- aoc/2019/09/main/main.go
+++ aoc/2019/09/main/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 )
 
 var samples = []string{
@@ -14,20 +12,11 @@ var samples = []string{
 
 func main() {
 	for _, input := range samples {
-		codes := parse(input)
-		mem := make([]int, 1024)
-		copy(mem, codes)
-		in, out := make(chan int), make(chan int)
-		p := &prog{
-			pc:     0,
-			rel:    0,
-			mem:    mem,
-			input:  in,
-			output: out,
-		}
-		go p.run()
+		codes := parseIntCodes(input)
+		m := NewIntMachine(codes)
+		go m.Run()
 		for {
-			v, ok := <-out
+			v, ok := <-m.Reader()
 			if !ok {
 				break
 			}
@@ -37,143 +26,18 @@ func main() {
 	}
 
 	for _, startVal := range []int{1, 2} {
-		codes := parse("99")
-		mem := make([]int, 16*1024)
-		copy(mem, codes)
-		in, out := make(chan int), make(chan int)
-		p := &prog{
-			pc:     0,
-			rel:    0,
-			mem:    mem,
-			input:  in,
-			output: out,
-		}
-		go p.run()
-		in <- startVal
+		codes := parseIntCodes(`99`)
+		m := NewIntMachine(codes)
+		go m.Run()
+
+		m.Writer() <- startVal
 		for {
-			v, ok := <-out
+			v, ok := <-m.Reader()
 			if !ok {
 				break
 			}
 			fmt.Println(v)
 		}
 		fmt.Println()
-	}
-}
-
-func parse(line string) []int {
-	vals := strings.Split(line, ",")
-	var codes []int
-	for _, v := range vals {
-		codes = append(codes, mustInt(v))
-	}
-	return codes
-}
-
-type prog struct {
-	pc     int // program counter
-	rel    int // relative base
-	mem    []int
-	input  <-chan int
-	output chan<- int
-}
-
-func (p *prog) run() {
-	defer close(p.output)
-	for {
-		op := p.mem[p.pc]
-		p.pc++
-		if op == 99 {
-			return
-		}
-		switch op % 100 {
-		case 1:
-			a := p.load(op / 100)
-			b := p.load(op / 1000)
-			c := p.addr(op / 10000)
-			*c = a + b
-		case 2:
-			a := p.load(op / 100)
-			b := p.load(op / 1000)
-			c := p.addr(op / 10000)
-			*c = a * b
-		case 3:
-			c := p.addr(op / 100)
-			*c = <-p.input
-		case 4:
-			a := p.load(op / 100)
-			p.output <- a
-		case 5:
-			a := p.load(op / 100)
-			b := p.load(op / 1000)
-			if a != 0 {
-				p.pc = b
-			}
-		case 6:
-			a := p.load(op / 100)
-			b := p.load(op / 1000)
-			if a == 0 {
-				p.pc = b
-			}
-		case 7:
-			a := p.load(op / 100)
-			b := p.load(op / 1000)
-			c := p.addr(op / 10000)
-			*c = boolVal(a < b)
-		case 8:
-			a := p.load(op / 100)
-			b := p.load(op / 1000)
-			c := p.addr(op / 10000)
-			*c = boolVal(a == b)
-		case 9:
-			a := p.load(op / 100)
-			p.rel += a
-		default:
-			panic(op)
-		}
-	}
-}
-
-func (p *prog) load(op int) int {
-	v := p.mem[p.pc]
-	p.pc++
-	switch op % 10 {
-	case 0:
-		return p.mem[v]
-	case 1:
-		return v
-	case 2:
-		return p.mem[v+p.rel]
-	default:
-		panic(op)
-	}
-}
-
-func (p *prog) addr(op int) *int {
-	a := p.mem[p.pc]
-	p.pc++
-	switch op % 10 {
-	case 0:
-		return &p.mem[a]
-	case 2:
-		return &p.mem[a+p.rel]
-	default:
-		panic(op)
-	}
-}
-
-func boolVal(v bool) int {
-	if v {
-		return 1
-	} else {
-		return 0
-	}
-}
-
-func mustInt(s string) int {
-	if v, err := strconv.Atoi(s); err != nil {
-		panic(fmt.Sprint(s, err))
-	} else {
-		return v
 	}
 }
