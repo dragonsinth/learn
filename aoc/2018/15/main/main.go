a42a481c424ace839493b7beb0ab65cc430b7f85
Scott Blum <dragonsinth@gmail.com>
2023-08-05 08:46:13 -0400

    aoc 2018 15

---
+++ aoc/2018/15/main/main.go
@@ -0,0 +1,218 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/dragonsinth/learn/aoc/termbox"
+)
+
+var samples = []string{`
+#######
+#.G...#
+#...EG#
+#.#.#G#
+#..G#E#
+#.....#
+#######
+`, `
+#######
+#G..#E#
+#E#E.E#
+#G.##.#
+#...#E#
+#...E.#
+#######
+`, `
+#######
+#E..EG#
+#.#G.E#
+#E.##E#
+#G..#.#
+#..E#.#
+#######
+`, `
+#######
+#E.G#.#
+#.#G..#
+#G.#.G#
+#G..#.#
+#...E.#
+#######
+`, `
+#######
+#.E...#
+#.#..G#
+#.###.#
+#E#G#G#
+#...#G#
+#######
+`, `
+#########
+#G......#
+#.E.#...#
+#..##..G#
+#...##..#
+#...#...#
+#.G...G.#
+#.....G.#
+#########
+`}
+
+var expect = []int{
+	27730,
+	36334,
+	39514,
+	27755,
+	28944,
+	18740,
+}
+
+var expect2 = []int{
+	4988,
+	29064,
+	31284,
+	3478,
+	6474,
+	1140,
+}
+
+func main() {
+	for i, s := range samples {
+		p := run(s, false)
+		sum := p.hpSum()
+		actual := p.turn * sum
+		fmt.Println(p.turn, p.hpSum(), actual)
+		if actual != expect[i] {
+			panic(actual)
+		}
+	}
+
+	for i, s := range samples {
+		p := run2(s, false)
+		sum := p.hpSum()
+		actual := p.turn * sum
+		fmt.Println(p.elfAtk, p.turn, p.hpSum(), actual)
+		if actual != expect2[i] {
+			panic(actual)
+		}
+	}
+}
+
+func run(sample string, debug bool) *puz {
+	p := parse(sample)
+	p.elfAtk = 3
+	p.debug = debug
+	p.term = termbox.New(false)
+	//p.Print()
+	for {
+		live := p.Tick()
+		if live {
+			p.PrintFrame()
+		} else {
+			p.term.Stop()
+			p.Print()
+			return &p
+		}
+	}
+}
+
+func run2(sample string, debug bool) *puz {
+	for atk := 4; true; atk++ {
+		p := parse(sample)
+		p.elfAtk = atk
+		p.debug = debug
+		p.term = termbox.New(false)
+		//p.Print()
+		nElf := p.nElf
+		for {
+			live := p.Tick()
+			if live {
+				p.PrintFrame()
+			} else {
+				p.term.Stop()
+				p.Print()
+				if nElf == p.nElf {
+					return &p
+				} else {
+					break
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func parse(input string) puz {
+	p := puz{
+		units: map[pos]*unit{},
+		walls: map[pos]bool{},
+		w:     -1,
+		h:     -1,
+	}
+
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		if p.w < 0 {
+			p.w = len(line)
+		} else if p.w != len(line) {
+			panic(line)
+		}
+		p.h++
+
+		for x, c := range []byte(line) {
+			pt := pos{x, p.h}
+			switch c {
+			case '#':
+				p.walls[pt] = true
+			case '.':
+			case 'E':
+				p.units[pt] = &unit{p: pt, hp: 200, typ: ELF}
+				p.nElf++
+			case 'G':
+				p.units[pt] = &unit{p: pt, hp: 200, typ: GOBLIN}
+				p.nGob++
+			default:
+				panic(string(c))
+			}
+		}
+	}
+	p.h++
+	return p
+}
+
+type pos struct {
+	x, y int
+}
+
+func (p pos) adjacent() [4]pos {
+	return [4]pos{
+		{p.x, p.y - 1},
+		{p.x - 1, p.y},
+		{p.x + 1, p.y},
+		{p.x, p.y + 1},
+	}
+}
+
+func readOrderPosLess(a, b pos) bool {
+	if a.y != b.y {
+		return a.y < b.y
+	}
+	return a.x < b.x
+}
+
+type wat byte
+
+const (
+	ELF    = wat('E')
+	GOBLIN = wat('G')
+)
+
+type unit struct {
+	p   pos
+	hp  int
+	typ wat
+}
---
+++ aoc/2018/15/main/puzzle.go
@@ -0,0 +1,265 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/dragonsinth/learn/aoc/termbox"
+	"math"
+	"os"
+	"time"
+
+	"golang.org/x/exp/maps"
+	"golang.org/x/exp/slices"
+)
+
+type puz struct {
+	units      map[pos]*unit
+	walls      map[pos]bool
+	w, h       int
+	nElf, nGob int
+	elfAtk     int
+	turn       int
+
+	term  termbox.Terminal
+	debug bool
+}
+
+func (p *puz) Tick() bool {
+	// run each unit in order
+	units := maps.Values(p.units)
+	slices.SortFunc(units, func(a, b *unit) bool {
+		return readOrderPosLess(a.p, b.p)
+	})
+
+	for _, u := range units {
+		if u.hp < 1 {
+			// must have died
+			continue
+		}
+
+		if p.nElf == 0 || p.nGob == 0 {
+			return false
+		}
+
+		if pt := p.Move(*u); pt != nil {
+			dst := *pt
+			if !p.empty(dst) {
+				panic(dst)
+			}
+			delete(p.units, u.p)
+			u.p = dst
+			p.units[u.p] = u
+		}
+
+		p.Attack(*u)
+	}
+
+	p.turn++
+	return true
+}
+
+func (p *puz) Attack(u unit) {
+	var targets []*unit
+	for _, pt := range u.p.adjacent() {
+		if foe := p.units[pt]; foe != nil && u.typ != foe.typ {
+			targets = append(targets, foe)
+		}
+	}
+	if len(targets) == 0 {
+		return
+	}
+	slices.SortFunc(targets, func(a *unit, b *unit) bool {
+		if a.hp != b.hp {
+			return a.hp < b.hp
+		}
+		if a.p.y != b.p.y {
+			return a.p.y < b.p.y
+		}
+		return a.p.x < b.p.x
+	})
+
+	t := targets[0]
+	switch u.typ {
+	case ELF:
+		t.hp -= p.elfAtk
+	case GOBLIN:
+		t.hp -= 3
+	}
+	if t.hp < 1 {
+		delete(p.units, t.p)
+		switch t.typ {
+		case ELF:
+			p.nElf--
+		case GOBLIN:
+			p.nGob--
+		default:
+			panic(t.typ)
+		}
+	}
+}
+
+func (p *puz) Move(u unit) *pos {
+	// Find all target points.
+	targets := make(map[pos]bool, p.nElf+p.nGob)
+	for _, foe := range p.units {
+		if u.typ == foe.typ {
+			continue // friend
+		}
+		for _, pt := range foe.p.adjacent() {
+			if pt == u.p {
+				// there's an enemy right next to me
+				return nil
+			}
+			if p.empty(pt) {
+				targets[pt] = true
+			}
+		}
+	}
+
+	if len(targets) == 0 {
+		return nil
+	}
+
+	type path []pos
+	work := make([]path, 0, 1024)
+	reachable := make(map[pos]path, len(targets))
+	dist := math.MaxInt
+	seen := make(map[pos]bool, p.w*p.h)
+	for _, pt := range u.p.adjacent() {
+		if p.empty(pt) {
+			work = append(work, path{pt})
+		}
+	}
+
+	for len(work) > 0 {
+		pth := work[0]
+		copy(work, work[1:])
+		work = work[:len(work)-1]
+		if len(pth) > dist {
+			break
+		}
+
+		last := pth[len(pth)-1]
+		if seen[last] {
+			continue
+		}
+		seen[last] = true
+
+		if targets[last] {
+			// This is a valid target we haven't reached yet!
+			reachable[last] = pth
+			dist = len(pth)
+			continue
+		}
+
+		// Consider any adjacent points.
+		for _, pt := range last.adjacent() {
+			if p.empty(pt) {
+				newPath := make(path, 0, len(pth)+1)
+				newPath = append(newPath, pth...)
+				newPath = append(newPath, pt)
+				work = append(work, newPath)
+			}
+		}
+	}
+
+	if len(reachable) == 0 {
+		return nil
+	}
+
+	// Pick the best reachable target in read order
+	reachPos := maps.Keys(reachable)
+	slices.SortFunc(reachPos, readOrderPosLess)
+	pth := reachable[reachPos[0]]
+
+	// walk that path
+	return &pth[0]
+}
+
+func (p *puz) Print() {
+	p.term.Render(p.Render(), os.Stdout)
+}
+
+func (p *puz) PrintFrame() {
+	if p.debug {
+		p.term.Render(p.Render(), os.Stdout)
+		if p.term.Enabled() {
+			p.wait()
+		}
+	}
+}
+
+func (p *puz) Render() [][]byte {
+	var buf [][]byte
+	for y := 0; y < p.h; y++ {
+		var line []byte
+		var rem bytes.Buffer
+		for x := 0; x < p.w; x++ {
+			var ch byte
+			pt := pos{x, y}
+			if u, ok := p.units[pt]; ok {
+				ch = byte(u.typ)
+				if rem.Len() == 0 {
+					rem.WriteString("   ")
+				} else {
+					rem.WriteString(", ")
+				}
+				rem.WriteByte(ch)
+				rem.WriteString(fmt.Sprintf("(%d)", u.hp))
+			} else if p.walls[pt] {
+				ch = '#'
+			} else {
+				ch = '.'
+			}
+			line = append(line, ch)
+		}
+		line = append(line, rem.Bytes()...)
+		buf = append(buf, line)
+	}
+	buf = append(buf, []byte(fmt.Sprintf("Turn %d", p.turn)))
+	return buf
+}
+
+func (p *puz) DebugRender(targets map[pos]bool, u *unit) [][]byte {
+	var buf [][]byte
+	for y := 0; y < p.h; y++ {
+		var line []byte
+		for x := 0; x < p.w; x++ {
+			var ch byte
+			pt := pos{x, y}
+			if u != nil && u.p == pt {
+				ch = byte(u.typ) - 'A' + 'a'
+			} else if u, ok := p.units[pt]; ok {
+				ch = byte(u.typ)
+			} else if p.walls[pt] {
+				ch = '#'
+			} else if targets[pt] {
+				ch = '?'
+			} else {
+				ch = '.'
+			}
+			line = append(line, ch)
+		}
+		buf = append(buf, line)
+	}
+	buf = append(buf, []byte(fmt.Sprintf("Turn %d", p.turn)))
+	return buf
+}
+
+func (p *puz) empty(pt pos) bool {
+	_, hasUnit := p.units[pt]
+	hasWall := p.walls[pt]
+	return !hasUnit && !hasWall
+}
+
+func (p *puz) wait() {
+	time.Sleep(100 * time.Millisecond)
+}
+
+func (p *puz) hpSum() int {
+	sum := 0
+	for _, u := range p.units {
+		sum += u.hp
+	}
+	return sum
+}
