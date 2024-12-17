23ef8911960327dd2b7d2d6a5c1ea2d668d3d661
Scott Blum <dragonsinth@gmail.com>
2024-12-17 18:36:32 -0500

    aoc 2024 17

---
+++ aoc/2024/17/main/main.go
@@ -0,0 +1,247 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+var (
+	sample1 = []byte{0, 1, 5, 4, 3, 0}
+	sample2 = []byte{0, 3, 5, 4, 3, 0}
+)
+
+func main() {
+	part1(sample1, 729)
+	fmt.Println()
+
+	dump(sample2)
+	fmt.Println()
+
+	part2(sample2)
+	fmt.Println()
+}
+
+func part1(ops []byte, a int) {
+	p := &Program{
+		A:   a,
+		B:   0,
+		C:   0,
+		IP:  0,
+		Ops: ops,
+		Out: nil,
+	}
+	fmt.Println(stringBytes(p.Run()))
+}
+
+func part2(ops []byte) {
+	best = ""
+	fmt.Println(findSolutionBits(ops, 0, 3))
+}
+
+type Program struct {
+	A   int
+	B   int
+	C   int
+	IP  int
+	Ops []byte
+	Out []byte
+}
+
+func (p *Program) Run() []byte {
+	ops := []func(byte){
+		0: p.adv,
+		1: p.bxl,
+		2: p.bst,
+		3: p.jnz,
+		4: p.bxc,
+		5: p.out,
+		6: p.bdv,
+		7: p.cdv,
+	}
+
+	for p.IP = 0; p.IP < len(p.Ops); p.IP += 2 {
+		code, op := p.Ops[p.IP], p.Ops[p.IP+1]
+		ops[code](op)
+	}
+	return p.Out
+}
+
+func (p *Program) adv(op byte) {
+	p.A = p.div(op)
+}
+
+func (p *Program) bxl(op byte) {
+	p.B = p.B ^ int(op)
+}
+
+func (p *Program) bst(op byte) {
+	p.B = p.combo(op) % 8
+}
+
+func (p *Program) jnz(op byte) {
+	if p.A != 0 {
+		p.IP = int(op)
+		p.IP -= 2
+	}
+}
+
+func (p *Program) bxc(_ byte) {
+	p.B = p.B ^ p.C
+}
+
+func (p *Program) out(op byte) {
+	p.Out = append(p.Out, byte(p.combo(op)%8))
+}
+
+func (p *Program) bdv(op byte) {
+	p.B = p.div(op)
+}
+
+func (p *Program) cdv(op byte) {
+	p.C = p.div(op)
+}
+
+func (p *Program) div(op byte) int {
+	return p.A / (1 << p.combo(op))
+}
+
+func (p *Program) combo(op byte) int {
+	switch op {
+	case 0, 1, 2, 3:
+		return int(op)
+	case 4:
+		return p.A
+	case 5:
+		return p.B
+	case 6:
+		return p.C
+	default:
+		panic(op)
+	}
+}
+
+func dump(ops []byte) {
+	opString := []string{
+		0: "adv",
+		1: "bxl",
+		2: "bst",
+		3: "jnz",
+		4: "bxc",
+		5: "out",
+		6: "bdv",
+		7: "cdv",
+	}
+
+	for ip := 0; ip < len(ops); ip += 2 {
+		code, op := ops[ip], ops[ip+1]
+		fmt.Println(ip, ":", opString[code], opDesc(code, op))
+	}
+}
+
+func opDesc(code byte, op byte) string {
+	switch code {
+	case 0, 6, 7:
+		if op < 4 {
+			return strconv.Itoa(1 << int(op))
+		}
+		reg := op - 4
+		return "2^" + "ABC"[reg:reg+1]
+	case 1, 3:
+		return strconv.Itoa(int(op))
+	case 2, 5:
+		if op < 4 {
+			return strconv.Itoa(int(op))
+		}
+		reg := op - 4
+		return "ABC"[reg:reg+1] + " % 8"
+	case 4:
+		return "-"
+	default:
+		panic(code)
+	}
+}
+
+var best string
+
+func findSolution(ops []byte, guess int) int {
+	if guess > 1<<60 {
+		return 0 // give up
+	}
+
+	p := &Program{
+		A:   guess,
+		B:   0,
+		C:   0,
+		IP:  0,
+		Ops: ops,
+		Out: nil,
+	}
+	sol := p.Run()
+
+	if !bytes.HasSuffix(ops, sol) {
+		return 0
+	}
+	if bytes.Equal(ops, sol) {
+		return guess
+	}
+
+	dbg := stringBytesValue(guess)
+	if len(dbg) > len(best) {
+		best = dbg
+		fmt.Println(dbg)
+	}
+
+	if ans := findSolutionBits(ops, guess, 3); ans != 0 {
+		return ans
+	}
+	if ans := findSolutionBits(ops, guess, 6); ans != 0 {
+		return ans
+	}
+	if ans := findSolutionBits(ops, guess, 9); ans != 0 {
+		return ans
+	}
+	if ans := findSolutionBits(ops, guess, 12); ans != 0 {
+		return ans
+	}
+
+	// give up
+	return 0
+}
+
+func findSolutionBits(ops []byte, guess int, bits int) int {
+	newGuess := guess << bits
+	for i := 0; i < 1<<bits; i++ {
+		next := newGuess | i
+		if next == 0 {
+			continue
+		}
+		if ans := findSolution(ops, next); ans != 0 {
+			return ans
+		}
+	}
+	return 0
+}
+
+func stringBytes(in []byte) string {
+	var out strings.Builder
+	for i, e := range in {
+		if i > 0 {
+			out.WriteRune(',')
+		}
+		out.WriteRune(rune('0' + e))
+	}
+	return out.String()
+}
+
+func stringBytesValue(v int) string {
+	var b []byte
+	for v > 0 {
+		b = append(b, byte(v%8))
+		v /= 8
+	}
+	slices.Reverse(b)
+	return stringBytes(b)
+}
